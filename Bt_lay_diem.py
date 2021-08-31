677449b093b321ad59ddd912104a12e290d6e7a2
Linh Nguyễn <chijoker123@gmail.com>
2021-08-31 13:04:31 +0700

    Ban Alpha nha :))

--- Bt_lay_diem.py
+++ Bt_lay_diem.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 from tkinter import filedialog
+from win32api import GetSystemMetrics
 import tkinter
 import os
 import re
@@ -27,22 +28,42 @@ combo.grid(column=1, row=1)
 
 # Ham xu ly anh!!!!!!!!!!!!!!!!!!!!
 def xulyanh(pic):
-    try:
-        img = cv2.imread(pic)
-        cv2.imshow('Old image', img)
-        image = face_recognition.load_image_file(pic)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        face_locations = face_recognition.face_locations(image)
-        a = len(face_locations)
-        i = 0
-        while i < a:
-            cv2.rectangle(image, (face_locations[i][3], face_locations[i][0]), (face_locations[i][1], face_locations[i][2]), (255, 0, 255), 2)
-            i = i + 1
-        cv2.imshow("Face location image", image)
-        cv2.waitKey(0)
-    except:
-        messagebox.showinfo("Lỗi", "Không tồn tại folder như vậy.")
+    # try:
+    img = cv2.imread(pic)
+    image = face_recognition.load_image_file(pic)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    face_locations = face_recognition.face_locations(image)
+    a = len(face_locations)
+    i = 0
+    while i < a:
+        cv2.rectangle(image, (face_locations[i][3], face_locations[i][0]), (face_locations[i][1], face_locations[i][2]), (255, 0, 255), 2)
+        i = i + 1
+    img = re_size(img)
+    cv2.imshow('Old image', img)
+    image = re_size(image)
+    cv2.imshow("Face location image", image)
+    cv2.waitKey(0)
+    # except:
+    #     messagebox.showinfo("Lỗi", "Không tồn tại folder như vậy.")
     return
+
+def re_size(image):
+    win_w = GetSystemMetrics(0)
+    win_h = GetSystemMetrics(1) 
+    w = image.shape[0]
+    h = image.shape[1]
+    fw = w/win_w
+    fh = h/win_h
+    ff = 0
+    if fw > fh:
+        ff = fw
+    else:
+        ff = fh
+    if ff > 1:
+        w = int(w / ff)
+        h = int(h / ff)
+    image = cv2.resize(image, (h, w))
+    return image
 
 def btnclick():
     global txtfolder
